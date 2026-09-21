152f039e53da323341888afa3ad44b2d332c54eb
sesetk <serenatakatai@icloud.com>
2026-09-21 18:53:53 +1100

    TP5 - PARTIE 2

--- server-express.mjs
+++ server-express.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import createError from "http-errors";
 
 const host = "localhost";
 const port = 8000;
@@ -11,7 +12,10 @@ if (app.get("env") === "development") app.use(morgan("dev"));
 app.use(express.static("static"));
 
 app.get("/random/:nb", async function (request, response, next) {
-  const length = request.params.nb;
+  const length = Number.parseInt(request.params.nb, 10);
+  if (Number.isNaN(length)) {
+    return next(createError(400));
+  }
   const numbers = Array.from({ length }).map((_) => Math.floor(100 * Math.random()));
   const welcome = "Voici vos nombres aléatoires";
   return response.render("random", { numbers, welcome });
